65acac7b76a6bc2b5474f77946f3c055c98547ad
Alexis <alexis.montorier@laposte.net>
2019-04-11 16:53:21 -0400

    finalisation de la météo avec ville en dur

--- Tip_Taxes/app/src/main/java/com/example/tip_taxes/meteo/Meteo.java
+++ Tip_Taxes/app/src/main/java/com/example/tip_taxes/meteo/Meteo.java
@@ -17,7 +17,8 @@ public class Meteo extends AppCompatActivity {
     ImageView image;
     String temps;
 
-    private float KELVIN = -273.15f;
+    private float CELSIUS_CONVERT = -273.15f;
+    private float KMH_CONVERT = 3.6f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,30 +31,37 @@ public class Meteo extends AppCompatActivity {
         pression = findViewById(R.id.pression);
         image = findViewById(R.id.image);
 
-        String city = "Chicoutimi,CA";
+        String city = "Chicoutimi,ca";
 
         JSONWeatherTask task = new JSONWeatherTask();
         task.execute(new String[]{city});
     }
 
     private void chooseImageSource() {
-        if(temps.equals("01d")){
+        if(temps.equals("01d") || temps.equals("01n")){
             image.setImageResource(R.drawable.soleil);
+            textTemp.setText(getString(R.string.soleil));
         }
-        else if(temps.equals("02d")){
+        else if(temps.equals("02d") || temps.equals("02n")){
             image.setImageResource(R.drawable.soleilnuageux);
+            textTemp.setText(getString(R.string.soleilnuageux));
         }
-        else if(temps.equals("03d")|| temps.equals("04d") || temps.equals("50d")){
+        else if(temps.equals("03d")|| temps.equals("04d") || temps.equals("50d") || temps.equals("03n")|| temps.equals("04n") || temps.equals("50n")){
             image.setImageResource(R.drawable.nuage);
+            textTemp.setText(getString(R.string.nuage));
         }
-        else if(temps.equals("09d") || temps.equals("10d")){
+        else if(temps.equals("09d") || temps.equals("10d") || temps.equals("09n") || temps.equals("10n")){
             image.setImageResource(R.drawable.pluvieux);
+            textTemp.setText(getString(R.string.pluvieux));
         }
-        else if(temps.equals("11d")){
+        else if(temps.equals("11d") || temps.equals("11n")){
             image.setImageResource(R.drawable.tonnerre);
+            textTemp.setText(getString(R.string.tonnerre));
+
         }
-        else if(temps.equals("13d")){
+        else if(temps.equals("13d") || temps.equals("13n")){
             image.setImageResource(R.drawable.neige);
+            textTemp.setText(getString(R.string.neige));
         }
     }
 
@@ -63,10 +71,6 @@ public class Meteo extends AppCompatActivity {
         protected Weather doInBackground(String[] params) {
             Weather weather = new Weather();
             String data = new WeatherHttpClient().getWeatherData(params[0]);
-
-            if(data == null){
-                Log.i("DEBUG", "data is null");
-            }
 
             try {
                 weather = ParserJSON.parse(data);
@@ -83,26 +87,15 @@ public class Meteo extends AppCompatActivity {
         protected void onPostExecute(Weather weather) {
             super.onPostExecute(weather);
 
-            float tmp = weather.temperature.getTemp() + KELVIN;
-/*            String temperatureString = Float.toString(tmp);*/
-            float windSpeed = weather.wind.getSpeed()*3.6f;
-            /*String windSpeed = Float.toString(tmp);*/
+            float tmp = weather.temperature.getTemp() + CELSIUS_CONVERT;
+            float windSpeed = weather.wind.getSpeed()*KMH_CONVERT;
 
             String city = weather.location.getCity();
             String icon = weather.currentCondition.getIcon();
-            String description = weather.currentCondition.getDescr();
             int pressure = weather.currentCondition.getPressure();
-
-/*            Log.i("TEST","ville : "+city);
-            Log.i("TEST","icon : "+icon);
-            Log.i("TEST", "description : "+description);
-            Log.i("TEST","temperature : "+temperatureString+" °C");
-            Log.i("TEST", "vent : "+ windSpeed);
-            Log.i("TEST", "pression : "+pressure);*/
 
             temps=icon;
             ville.setText(city);
-            textTemp.setText(description);
             temperature.setText(String.format("Température : %.1f °C", tmp));
             vent.setText(String.format("Vent : %.1f Km/h", windSpeed));
             pression.setText(String.format("Pression : %d hPa", pressure));
