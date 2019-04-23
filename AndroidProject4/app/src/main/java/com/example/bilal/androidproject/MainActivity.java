0de0b54ef7b2720c8a0315bf0fac8a0468b39be3
Alexis <alexis.montorier@laposte.net>
2019-04-23 00:16:38 -0400

    modification description fragment météo

--- AndroidProject4/app/src/main/java/com/example/bilal/androidproject/MainActivity.java
+++ AndroidProject4/app/src/main/java/com/example/bilal/androidproject/MainActivity.java
@@ -22,8 +22,6 @@ public class MainActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new MyPageAdapter (getSupportFragmentManager());
